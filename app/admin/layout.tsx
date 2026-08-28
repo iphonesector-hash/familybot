c62f48447b0a212c4d0fe5b2b96431a4e4e9c421
iphonesector-hash <iphone.sector@gmail.com>
2026-08-28 23:12:19 +0330

    security: bootstrap admin from current Bale identity

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,4 +1,5 @@
 import ReminderShortcut from "./ReminderShortcut";
 import OwnerGiftShortcut from "./OwnerGiftShortcut";
+import AdminSessionBootstrap from "./AdminSessionBootstrap";
 
-export default function AdminLayout({children}:{children:React.ReactNode}){return <>{children}<ReminderShortcut/><OwnerGiftShortcut/></>}
+export default function AdminLayout({children}:{children:React.ReactNode}){return <><AdminSessionBootstrap/>{children}<ReminderShortcut/><OwnerGiftShortcut/></>}
