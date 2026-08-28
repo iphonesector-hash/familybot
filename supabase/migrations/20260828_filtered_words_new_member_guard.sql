d4a2d9dce1e7c01207d15cc6a066c015bf57d3d2
iphonesector-hash <iphone.sector@gmail.com>
2026-08-29 00:08:27 +0330

    db: remove legacy short moderation guard migration

--- supabase/migrations/20260828_filtered_words_new_member_guard.sql
+++
@@ -1,4 +0,0 @@
-alter table group_settings add column if not exists filtered_words text[] not null default '{}';
-alter table group_settings add column if not exists new_member_restrict_minutes integer not null default 0;
-alter table group_settings drop constraint if exists group_settings_new_member_restrict_minutes_check;
-alter table group_settings add constraint group_settings_new_member_restrict_minutes_check check (new_member_restrict_minutes between 0 and 10080);
