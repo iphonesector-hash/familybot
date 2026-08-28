7153beab3d365f1cd0448136bbd404d6fda6037a
iphonesector-hash <iphone.sector@gmail.com>
2026-08-28 23:01:45 +0330

    feat: add owner gift shortcut

---
+++ app/admin/OwnerGiftShortcut.tsx
@@ -0,0 +1,3 @@
+"use client";
+import Link from "next/link";
+export default function OwnerGiftShortcut(){return <Link href="/admin/owner-gifts" className="primaryCta" style={{position:"fixed",left:18,bottom:88,zIndex:30,boxShadow:"0 10px 35px rgba(91,48,180,.45)"}}>🎁 هدیه Owner</Link>}
